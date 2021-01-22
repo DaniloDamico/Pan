39a4c0e097e1c8d90b66583c21a0bb715a56dba8
DaniloDamico <45599533+DaniloDamico@users.noreply.github.com>
2021-01-23 00:03:35 +0100

    added dummy java project

---
+++ src/test/TestHelloWorld.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.Test;
+import logic.HelloWorld;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testHelloWorldResult() {
+		// not a real test as Hello World just prints on the console
+		HelloWorld test = new HelloWorld();
+	}
+	
+}
