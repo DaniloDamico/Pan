d52020f1dfd517e0ec9594c0517ddb102f9ab7df
Danilo D'Amico <45599533+DaniloDamico@users.noreply.github.com>
2021-01-23 19:57:41 +0100

    added JavaFX support

---
+++ src/logic/JavaFXDummy.java
@@ -0,0 +1,16 @@
+package logic;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFXDummy extends Application {
+	public static void main(String[] args) {
+			launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception{
+		stage.show();
+	}
+}
+
